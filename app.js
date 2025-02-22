8199a50228bf03e7b5723c151b1e11bfafb8bd7a
Amanda Pontes <amandapontes.com@gmail.com>
2025-02-22 18:38:58 -0300

    Alterando limite para 100

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let listaNumeroSorteado = [];
-let numeroLimiteSorteado = 10;
+let numeroLimiteSorteado = 100;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativa = 1;
 
@@ -13,7 +13,7 @@ function exibirTextoNaTela(tag, texto){
 
 function exibirMensagemInicial(){
     exibirTextoNaTela('h1', 'Jogo do Número Secreto');
-    exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 100');
 
 }
 
